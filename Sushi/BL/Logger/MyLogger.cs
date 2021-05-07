aea0b29aa21c7fa9f48d70e7e5e31bb3c944c414
Сергей Лесько <se-ey@mail.ru>
2021-05-08 00:21:36 +0300

    1

--- Sushi/BL/Logger/MyLogger.cs
+++ Sushi/BL/Logger/MyLogger.cs
@@ -9,11 +9,12 @@ namespace Sushi.BL.Logger
         public void Info(string message, string method)
         {
             StreamWriter sw;
-            FileInfo log_file = new FileInfo(@"Sushi\BL\Logger\log"+ DateTime.Now.ToString("yyyy MM dd") +"_"+ counter + ".txt");
+            FileInfo log_file = new FileInfo(@"D:\C#\Project\Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") +"_"+ counter + ".txt");
+            log_file.Create().Close();
             long fileByteSize = log_file.Length;
             if (fileByteSize >= 30720) 
-                File.Move(@"Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt",
-                    @"Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter++ + ".txt");
+                File.Move(@"D:\C#\Project\Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt",
+                    @"D:\C#\Project\Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter++ + ".txt");
             sw = log_file.AppendText();
             sw.WriteLine(DateTime.Now.ToString("HH:mm:ss\t")+ "INFO " + $"[{method}]: " + message);
             sw.Close();
@@ -21,11 +22,12 @@ namespace Sushi.BL.Logger
         public void Debag(string message, string method)
         {
             StreamWriter sw;
-            FileInfo log_file = new FileInfo(@"Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt");
+            FileInfo log_file = new FileInfo(@"D:\C#\Project\Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt");
+            log_file.Create();
             long fileByteSize = log_file.Length;
             if (fileByteSize >= 30720)
-                File.Move(@"Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt",
-                    @"Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter++ + ".txt");
+                File.Move(@"D:\C#\Project\Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt",
+                    @"D:\C#\Project\Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter++ + ".txt");
             sw = log_file.AppendText();
             sw.WriteLine(DateTime.Now.ToString("HH:mm:ss\t") + "DEBAG " + $"[{method}]: " + message);
             sw.Close();
@@ -33,7 +35,8 @@ namespace Sushi.BL.Logger
         public void Error(string message, string method)
         {
             StreamWriter sw;
-            FileInfo log_file = new FileInfo(@"Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt");
+            FileInfo log_file = new FileInfo(@"D:\C#\Project\Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt");
+            log_file.Create();
             long fileByteSize = log_file.Length;
             if (fileByteSize >= 30720)
                 File.Move(@"Sushi\BL\Logger\log" + DateTime.Now.ToString("yyyy MM dd") + "_" + counter + ".txt",
--- Sushi/BL/Sushi/Repository/SushiRepository.cs
+++ Sushi/BL/Sushi/Repository/SushiRepository.cs
@@ -17,13 +17,7 @@ namespace Sushi.BL.Sushi.Repository
                 JsonSerializer serializer = new JsonSerializer();
                 List<SushiProp> sushiProps = (List<SushiProp>)serializer
                     .Deserialize(file, typeof(List<SushiProp>));
-                _sushiProps.AddRange(sushiProps);
-            }
-            _sushiProps.Add(sushiProp);
-            using (StreamWriter file = File.CreateText(Constant.WayToSushi))
-            {
-                JsonSerializer jsonSerializer = new JsonSerializer();
-                jsonSerializer.Serialize(file, _sushiProps);
+                sushiProps.AddRange(sushiProps);
             }
             return _sushiProps;
         }
@@ -34,15 +28,9 @@ namespace Sushi.BL.Sushi.Repository
                 JsonSerializer serializer = new JsonSerializer();
                 List<SushiProp> sushiProps = (List<SushiProp>)serializer
                     .Deserialize(file, typeof(List<SushiProp>));
-                _sushiProps.AddRange(sushiProps);
-            }
-            var item = _sushiProps.FirstOrDefault(x => x.SushiName == name);
-            _sushiProps.Remove(item);
-            return item;
-            using (StreamWriter file = File.CreateText(Constant.WayToSushi))
-            {
-                JsonSerializer jsonSerializer = new JsonSerializer();
-                jsonSerializer.Serialize(file, _sushiProps);
+                var item = _sushiProps.FirstOrDefault(x => x.SushiName == name);
+                sushiProps.Remove(item);
+                return item;
             }
         }
         public SushiProp Update(string name)
@@ -52,53 +40,48 @@ namespace Sushi.BL.Sushi.Repository
                 JsonSerializer serializer = new JsonSerializer();
                 List<SushiProp> sushiProps = (List<SushiProp>)serializer
                     .Deserialize(file, typeof(List<SushiProp>));
-                _sushiProps.AddRange(sushiProps);
-            }
-            var item = _sushiProps.FirstOrDefault(x => x.SushiName == name);
-            int userChoice = 6;
-            while (userChoice != 0)
-            {
-                Console.WriteLine("Что желате поменять? \n " +
-                    "1. Название суши \n " +
-                    "2. Описание  \n " +
-                    "3. Колличество суши \n " +
-                    "4. Вес суши \n " +
-                    "5. Цену \n " +
-                    "0. Все сделано");
-                int.TryParse(Console.ReadLine(), out userChoice);
-                switch (userChoice)
+                var item = _sushiProps.FirstOrDefault(x => x.SushiName == name);
+                int userChoice = 6;
+                while (userChoice != 0)
                 {
-                    case 1:
-                        Console.WriteLine("Введите новое название:");
-                        item.SushiName = Console.ReadLine();
-                        break;
-                    case 2:
-                        Console.WriteLine("Введите описание:");
-                        item.Description = Console.ReadLine();
-                        break;
-                    case 3:
-                        Console.WriteLine("Введите новое колличество суши:");
-                        item.NumberOfRolls = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 4:
-                        Console.WriteLine("Введите вес суши");
-                        item.Weight = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 5:
-                        Console.WriteLine("Введите новую цену на данные суши");
-                        item.Price = Convert.ToDouble(Console.ReadLine());
-                        break;
-                    default:
-                        userChoice = 0;
-                        break;
+                    Console.WriteLine("Что желате поменять? \n " +
+                        "1. Название суши \n " +
+                        "2. Описание  \n " +
+                        "3. Колличество суши \n " +
+                        "4. Вес суши \n " +
+                        "5. Цену \n " +
+                        "0. Все сделано");
+                    int.TryParse(Console.ReadLine(), out userChoice);
+                    switch (userChoice)
+                    {
+                        case 1:
+                            Console.WriteLine("Введите новое название:");
+                            item.SushiName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Введите описание:");
+                            item.Description = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Введите новое колличество суши:");
+                            item.NumberOfRolls = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 4:
+                            Console.WriteLine("Введите вес суши");
+                            item.Weight = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 5:
+                            Console.WriteLine("Введите новую цену на данные суши");
+                            item.Price = Convert.ToDouble(Console.ReadLine());
+                            break;
+                        default:
+                            userChoice = 0;
+                            break;
+                    }
+
                 }
+                return item;
             }
-            using (StreamWriter file = File.CreateText(Constant.WayToSushi))
-            {
-                JsonSerializer jsonSerializer = new JsonSerializer();
-                jsonSerializer.Serialize(file, _sushiProps);
-            }
-            return item;
         }
     }
 }
